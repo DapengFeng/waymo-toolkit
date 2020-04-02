23bb4a0654d028f06bfaeedfc7296945615ab55b
DapengFeng <dpengfeng@gmail.com>
2020-04-02 11:59:20 +0800

    update generate_protos.sh

--- generate_protos.sh
+++ generate_protos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-protoc -I=. --python_out=. waymo_extractor/protos/label.proto
-protoc -I=. --python_out=. waymo_extractor/protos/dataset.proto
-protoc -I=. --python_out=. waymo_extractor/protos/annotation.proto 
+protoc -I=. --python_out=. waymo_toolkit/protos/label.proto
+protoc -I=. --python_out=. waymo_toolkit/protos/dataset.proto
+protoc -I=. --python_out=. waymo_toolkit/protos/annotation.proto 
